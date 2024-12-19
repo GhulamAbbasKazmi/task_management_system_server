7c4609bfa595bac5fa0d5a677eda23a920577e46
GhulamAbbasKazmi <ghulama16@gmail.com>
2024-12-20 02:10:41 +0500

    vercel updates 1

--- server.js
+++ server.js
@@ -1,27 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
 const cors = require('cors');
 require('dotenv').config(); 
 
 const userRoutes = require('./routes/userRoutes');
-const taskItemRoutes = require("./routes/taskItemRoutes");
-
-
+const taskItemRoutes = require('./routes/taskItemRoutes');
 
 const app = express();
+
+// Allow CORS for all origins
 app.use(cors());
+
+// Body parser middleware
 app.use(express.json());
 
-
+// Connect to MongoDB
 mongoose.connect(process.env.DB)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
 
-
-
 // Use the routes
-app.use('/tasks', taskItemRoutes)
+app.use('/tasks', taskItemRoutes);
 app.use('/users', userRoutes);
 
 // Listen on dynamic port or default to 5000 for local development
